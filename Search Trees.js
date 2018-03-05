4f3e089143f06283c58fb293c3b9dea6f856da00
Jafsari <justinafsari@yahoo.com>
2018-03-05 11:41:45 -0800

    adding search tree

---
+++ Search Trees.js
@@ -0,0 +1,32 @@
+// --- Directions
+// 1) Create a node class.  The constructor
+// should accept an argument that gets assigned
+// to the data property and initialize an
+// empty array for storing children. The node
+// class should have methods 'add' and 'remove'.
+// 2) Create a tree class. The tree constructor
+// should initialize a 'root' property to null.
+// 3) Implement 'traverseBF' and 'traverseDF'
+// on the tree class.  Each method should accept a
+// function that gets called with each element in the tree
+
+class Node { //Creating the node
+    constructor(data){
+      this.data = data;
+      this.children = [];
+    }
+    add(data) { //adding a new node with push method
+      this.children.push(new Node(data))
+    }
+    remove(data){ //Using the built-in array filter method 
+      this.children = this.children.filter(node => {
+        return node.data !== data;
+      });
+    }
+  }
+
+class Tree {
+    constructor(){
+        this.root = null
+    }
+}
