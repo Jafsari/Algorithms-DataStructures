35664cfb2a2602e877179864eeb28f0e8e1c59ce
Jafsari <justinafsari@yahoo.com>
2018-03-06 14:52:19 -0800

    recursion

---
+++ recursionPractice.js
@@ -0,0 +1,55 @@
+function loop(base,expo){
+    let count = 0;
+    let result = [];
+    while ( count < expo) {
+      count++
+      result.push(base)
+    }
+    return result.reduce((a,b) => {
+      return a * b
+    })
+  }
+  
+  function recurse(base,expo){
+    if (expo === 1) {
+      return base;
+    }
+    return base * recurse(base,expo-1)
+  }
+  
+  function reverseArrloop(arr){
+    return arr.reverse()
+  }
+  
+  function reverseArr(arr){
+    let result = [];
+    function help(){
+      if (arr.length > 0){
+        result.push(arr.pop())
+        help()
+      }
+    }
+    help()
+    return result;
+  }
+  
+  function reverseString(str){
+      return str.split('').reverse().join('')
+  }
+  
+  function reverseString(str){
+   let result = "";
+   let count = str.length
+   function help(){
+     if (str.length > 0){
+       count--
+       result += str[str.length-1]
+       str = str.slice(0,count)
+       help()
+     }
+   }
+   help()
+   return result
+  }
+  
+  reverseString('justin')
