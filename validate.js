3f4c5fe6b1356c1851a1c2f87737f94205f63cf1
Jafsari <justinafsari@yahoo.com>
2018-03-06 11:15:23 -0800

    adding validate

---
+++ validate.js
@@ -0,0 +1,25 @@
+class Node {
+    constructor(data) {
+      this.data = data;
+      this.left = null;
+      this.right = null;
+    }
+  }
+
+
+
+function validate(node, min = null, max = null) {
+    if (max !== null && node.data > max){
+        return false;
+    }
+    if (min !== null && node.data < min){
+        return false;
+    }
+   if (node.left && !validate(node.left,min,node.data)){
+       return false;
+   }
+   if (node.right && !validate(node.right,node.data,max)){
+       return false;
+   }
+   return true;
+ }
