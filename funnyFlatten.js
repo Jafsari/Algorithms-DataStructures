a0f04283dfc3b518d7e41f7f25fcc0409d846e84
Jafsari <justinafsari@yahoo.com>
2018-03-15 18:10:12 -0700

    adding recursive flatten

--- funnyFlatten.js
+++ funnyFlatten.js
@@ -23,3 +23,18 @@ function funnyFlatten(arr){
     }
     
     funnyFlatten([[2],[[[[[1]]]]],[[3]],[[[[[4]]]]],[[[[[[[[[5]]]]]]]]]])
+
+// recursive flatten
+    function flatten(arr) {
+        var container = [];
+        for(var i = 0; i < arr.length; i++) {
+            if(Array.isArray(arr[i])) {
+                container = container.concat(flatten(arr[i]));
+            } else {
+                container.push(arr[i]);
+            }
+        }
+        return container;
+    }
+    
+    flatten([[1],[[2],[[3]],[4]],5])
