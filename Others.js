986f67cfe28a009cb06b0763a56337cbdebec42e
Jafsari <justinafsari@yahoo.com>
2018-03-17 12:05:28 -0700

    adding others

---
+++ Others.js
@@ -0,0 +1,21 @@
+function tripleAdd(num1){
+    return function(num2){
+      return function(num3){
+        return num1 + num2 + num3
+      }
+    }
+  }
+  
+  tripleAdd(10)(20)(30)
+
+  
+
+  function quadrupleAdd(num1) {
+    return function(num2) {
+      return function(num3, num4) {
+        return num1 + num2 + num3 + num4;
+      };
+    };
+  }
+   
+  quadrupleAdd(10)(20)(30, 40);
