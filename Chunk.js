477c6eb33e41e4490e26984a8f846cef9753cd0c
Jafsari <justinafsari@yahoo.com>
2018-03-18 16:20:48 -0700

    adding more chunk

--- Chunk.js
+++ Chunk.js
@@ -6,3 +6,12 @@ function chunk(array, size) {
      return container;
 }
 chunk([1,2,3,4,5],2)
+
+function chunkReverse(array, size) {
+    const container = [];
+    for(let i = 0; i < array.length; i += size){
+        container.push(array.slice(i, i+size).reverse());
+     }
+     return container;
+}
+chunkReverse([1,2,3,4,5],2)
