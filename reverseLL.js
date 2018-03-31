fb8be7d5f93ff1f71fb2b1b90911e9d5778bae7f
Jafsari <justinafsari@yahoo.com>
2018-03-31 16:57:22 -0700

    adding reversal

---
+++ reverseLL.js
@@ -0,0 +1,14 @@
+reverse(){
+    let current = this.head;
+    let next = current.next
+    let prev = null;
+    while (current){
+      current.next = prev;
+      prev = current;
+      current = next;
+      if (next !== null){
+        next = next.next
+      }
+    }
+    this.head = prev;
+  }
