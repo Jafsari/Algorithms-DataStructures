d7d2e3d9d8d67b426ffe0132030285a0d2632f0a
Jafsari <justinafsari@yahoo.com>
2018-04-02 21:54:41 -0700

    adding arraystacks

---
+++ ArrayStacks.js
@@ -0,0 +1,6 @@
+/* To implement an array with three stacks,
+I want to first take the length of the suppose array into consideration.
+I want to then create the stacks to be relevant towards the array's length.
+I want to then define the start of each stack.
+And then I want to be able to have functionality within each stack
+*/
