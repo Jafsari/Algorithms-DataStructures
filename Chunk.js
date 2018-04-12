189827e1be856ce7ee743a0658070da2421ec18b
Jafsari <justinafsari@yahoo.com>
2018-04-11 23:34:46 -0700

    adding recursion solution

--- Chunk.js
+++ Chunk.js
@@ -15,3 +15,21 @@ function chunkReverse(array, size) {
      return container;
 }
 chunkReverse([1,2,3,4,5],2)
+
+
+function chunkRecurse(arr,size){
+    var container = [];
+    var count = 0;
+    var length = arr.length
+    var a = arr
+    function inner (arr){
+      if (count < length){
+        container.push(a.slice(count,count+size).reverse());
+        count+=size
+        inner();
+      }
+      
+    }
+    inner(arr);
+    return [].concat(...container);
+  }
